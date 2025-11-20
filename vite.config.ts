9963979386da172d5c33ca3385eab49aa715cf51
root <root@LGsata.localdomain>
2025-11-20 11:17:18 +0800

    feat: 添加代理

--- vite.config.ts
+++ vite.config.ts
@@ -15,4 +15,25 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
+  server: {
+    host: '0.0.0.0',
+    port: 5173,
+    proxy: {
+      '/api/usercenter': {
+        target: 'http://localhost:3081',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+      '/api/llm': {
+        target: 'http://localhost:3082',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+      '/api/voice': {
+        target: 'http://localhost:3083',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    }
+  }
 })
